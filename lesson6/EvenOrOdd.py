21f7ffda5204849ec52671858025cfd71daeb197
Eric Yin <tiancai.yin@siemens.com>
2020-07-11 19:11:00 -0700

    add codes for cs201 lesson6

---
+++ lesson6/EvenOrOdd.py
@@ -0,0 +1,13 @@
+
+
+    if (m % 2 == 0):  # tests if m is even
+
+        # do even stuff
+        
+    else:             # otherwise, m must be odd
+
+        # do odd  stuff
+
+   
+
+
---
+++ lesson6/IfCircle.py
@@ -0,0 +1,10 @@
+answer = input("Do you want to see a circle? y/n:")
+if answer == 'y':
+    print("Working...")
+    import turtle
+    t = turtle.Pen()
+    t.speed(0)
+    t.circle(100) 
+print("Okay, we're done!")
+
+
