c061ba00b1d6b445c6a57ed148fc3ee923caa5f1
Eric Yin <tiancai.yin@siemens.com>
2020-09-11 19:53:54 -0700

    add lecture3

---
+++ lecture3/CelsiusToFahrenheit.py
@@ -0,0 +1,7 @@
+# Math Furmula: (0C * 9 / 5) + 32 = 32F
+strCelsius = input("Enter Celsius:")
+floatCelsius = float(strCelsius)
+
+floatFahrenheit = (floatCelsius * (9 / 5)) + 32
+
+print(floatFahrenheit)
